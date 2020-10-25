93d98d3dd7dde3cdd01f5e54c584c43ee692fda0
Carlos Esteban <carlosbcn169@gmail.com>
2020-10-25 12:41:14 +0100

    Add migrations

---
+++ app/Course.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Course
+ *
+ * @property int $id
+ * @property int $teacher_id
+ * @property int $category_id
+ * @property int $level_id
+ * @property string $name
+ * @property string $description
+ * @property string $slug
+ * @property string|null $picture
+ * @property string $status
+ * @property int $previous_approved
+ * @property int $previous_rejected
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Course newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Course newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Course query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereLevelId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course wherePicture($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course wherePreviousApproved($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course wherePreviousRejected($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereTeacherId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Course extends Model
+{
+    const PUBLISHED = 1;
+    const PENDING = 2;
+    const REJECTED = 3;
+    
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -28,13 +28,34 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('picture')->nullable();
+            //Cashier
             $table->string('stripe_id')->nullable();
             $table->string('card_brand')->nullable();
             $table->string('card_last_four')->nullable();
             $table->string('trial_ends_at')->nullable();
-
             $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->string('stripe_id');
+            $table->string('stripe_plan');
+            $table->integer('quantity');
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('user_social_accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('provider');
+            $table->string('provider_uid');
         });
     }
 
@@ -45,6 +66,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('user_social_accounts');
+        Schema::dropIfExists('subscriptions');
         Schema::dropIfExists('users');
         Schema::dropIfExists('roles');
     }
